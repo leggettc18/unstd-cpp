f89a1dc14c5e972d255798fc9431c925b43eb39a
Christopher Leggett <chris@leggett.dev>
2023-01-30 13:25:28 -0500

    Adds Array iterators

--- apps/demo.cpp
+++ apps/demo.cpp
@@ -10,8 +10,13 @@ int main(int argc, const char* argv[]) {
     data[2] = 2;
     data[3] = 3;
     data[4] = 4;
+    std::cout << "Manual Loop..." << std::endl;
     for (int i = 0; i < data.Size(); i++) {
         std::cout << data[i] << std::endl;
+    }
+    std::cout << "Iterators..." << std::endl;
+    for (auto& value : data) {
+        std::cout << value << std::endl;
     }
     
     std::cin.get();
--- include/unstd/Array.hpp
+++ include/unstd/Array.hpp
@@ -1,5 +1,5 @@
-
 #include <stddef.h>
+#include <iterator>
 namespace unstd {
 template <class T, size_t S>
 class Array {
@@ -12,5 +12,26 @@ public:
 
     T& operator[](size_t index) { return mData[index]; }
     const T& operator[](size_t index) const { return mData[index]; }
+
+    struct Iterator {
+        using iterator_category = std::forward_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = T;
+        using pointer = T*;
+        using reference = T&;
+        Iterator(pointer ptr) : mPtr(ptr) {}
+        reference operator*() const { return *mPtr; }
+        pointer operator->() { return mPtr; }
+        Iterator& operator++() { mPtr++; return *this; }
+        Iterator operator++(T) { Iterator tmp = *this; ++(*this); return tmp; }
+        friend bool operator== (const Iterator& a, const Iterator& b) { return a.mPtr == b.mPtr; };
+        friend bool operator != (const Iterator& a, const Iterator& b) { return a.mPtr != b.mPtr; };
+
+    private:
+        pointer mPtr;
+    };
+
+    Iterator begin() { return Iterator(&mData[0]); }
+    Iterator end() { return Iterator(&mData[this->Size()]); }
 };
 }
